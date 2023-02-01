44356a28867b197e9d6ca413834cd87c84c48318
Alina Robertson <alinanrobertson94@gmail.com>
2023-02-01 14:36:04 -0500

    part 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,10 @@ head(mechaCarData) #display first rows of imported data
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=mechaCarData) #generate multiple linear regression model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=mechaCarData)) #generate summary statistics
+
+suspension_coil_data <- read_csv("Resources/Suspension_Coil.csv") #import suspension coil data
+head(suspension_coil_data)
+
+total_summary <- suspension_coil_data %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create total summary table
+
+lot_summary <- suspension_coil_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create lot summary table
